2a3aed64f437c09f9438eb91507171c249999081
Steven <steven@stevenhill.co.za>
2021-05-29 15:23:23 +0200

    testing

--- src/js/tictactoe.js
+++ src/js/tictactoe.js
@@ -1,73 +1,30 @@
-const getSquares = () => {
-  return document.getElementsByClassName('inner-square');
-};
+const selectSquare = () => {
+  const squares = document.getElementsByClassName('inner-square');
 
-// passing getSquares() as argument
-const selectSquare = (squares) => {
   for (let square of squares) {
     square.addEventListener('click', () => addImageToSquare(square));
   }
 };
 
-// calling in selectSquare()
 const addImageToSquare = (square) => {
   const nought = document.querySelector('.nought');
   const cross = document.querySelector('.cross');
-  const noughtsTurn = 'noughtsTurn';
-  const crossesTurn = 'crossesTurn';
   const turn = playerTurn();
   let newGame = true;
 
-  if (turn === noughtsTurn || newGame === true) {
+  if (turn || newGame === true) {
     if (!cross) {
-      square.classList.add('nought');
-      endTurn(noughtsTurn);
-    }
-  }
-  if (turn === crossesTurn) {
-    if (!nought) {
-      square.classList.add('cross');
-      endTurn(crossesTurn);
+      square.classList.add(turn ? 'nought' : 'cross', 'checked');
     }
   }
 
   newGame = false;
 };
 
-// calling in addImageToSquare()
 const playerTurn = () => {
-  return switchTurns(endTurn());
-};
-
-// calling in playerTurn()
-const switchTurns = (endTurn) => {
-  const noughtEndsTurn = 'noughtEndsTurn';
-  const crossEndsTurn = 'crossEndsTurn';
-  const noughtsTurn = 'noughtsTurn';
-  const crossesTurn = 'crossesTurn';
-
-  if (noughtEndsTurn === endTurn) {
-    return crossesTurn;
-  }
-  if (crossEndsTurn === endTurn) {
-    return noughtsTurn;
+  const checked = document.querySelector('.checked');
+  if (checked) {
   }
 };
 
-// calling in playerTurn()
-const endTurn = (placedImage) => {
-  const noughtsTurn = 'noughtsTurn';
-  const crossesTurn = 'crossesTurn';
-  const noughtEndsTurn = 'noughtEndsTurn';
-  const crossEndsTurn = 'crossEndsTurn';
-
-  if (placedImage === noughtsTurn) {
-    return noughtEndsTurn;
-  }
-  if (placedImage === crossesTurn) {
-    return crossEndsTurn;
-  }
-};
-
-const squares = getSquares();
-selectSquare(squares);
+selectSquare();
